78f4d1d513a62fcd58c5996f65cd0bb80573cc5d
Romil003 <romilghadge33@gmail.com>
2022-12-05 20:03:39 +0530

    Added Employee payroll app Home Page UC4

---
+++ site_properties.js
@@ -0,0 +1,4 @@
+let site_properties = {
+    home_page : "../RFP Day 45 Assignments - Employee Payroll App Home Page/new_home.js",
+    add_emp_payroll_page: "../RFP Day 45 Assignments - Employee Payroll App Home Page/new_payroll_form_js.html"
+};
