fb2f9f5f9dc8134c326518ac880a9f6a4840a635
Romil003 <romilghadge33@gmail.com>
2022-12-05 20:50:55 +0530

    Added Employee payroll app HomePageUC5.2

--- new_home.js
+++ new_home.js
@@ -5,21 +5,24 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" + 
                         "<th>Salary</th><th>Start Date</th><th>Actions</th>";
-    let empPayrollData = createEmployeePayrollJSON()[0];                    
-    const innerHtml = `${headerHtml}
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for (const empPayrollData of empPayrollList) {                    
+    innerHtml = `${innerHtml}
      <tr>
-      <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -2.png"></td>
+      <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
       <td>${empPayrollData._name}</td>
       <td>${empPayrollData._gender}</td>
       <td>${getDeptHtml(empPayrollData._department)}</div>
       <td>${empPayrollData._salary}</td>
-      <td>${empPayrollData._gender}</td>
+      <td>${empPayrollData._startDate}</td>
       <td>
          <img name="${empPayrollData._id}" onclick="remove(this)" alt="delete"src="../assets/icons/delete-black-18dp.svg">
          <img name="${empPayrollData._id}" alt="edit" onclick="update(this)"src="../assets/icons/create-black-18dp.svg">
       </td>    
-</tr>
+    </tr>
     `;
+    }
     document.querySelector('#table-display').innerHTML = innerHtml;
 }
 
